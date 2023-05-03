e26774a22ec2a75bed32f0603b26903bdbddcd95
James Xu <james.xu@berkeley.edu>
2023-05-03 20:06:22 +0000

    adding the utils function

---
+++ analysis/utils.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from seaborn import load_dataset
