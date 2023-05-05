f74836e9ae26a7c500e0380f6e5b550cbd72d987
Nancy Xu <yinuoxu54@berkeley.edu>
2023-05-05 06:49:28 +0000

    updated utils.py with 3 functions

--- analysis/utils.py
+++ analysis/utils.py
@@ -6,6 +6,14 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from seaborn import load_dataset
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import metrics
 
 
 def clean_data(raw_data):
@@ -31,3 +39,100 @@ def clean_data(raw_data):
     df = pd.DataFrame(x_scaled, columns = modified_df.columns)
     features = df.columns[:-1]
     return df[features], df['HeartDisease']
+
+
+def fit_models(x_train, y_train, x_test, y_test):
+    
+    '''
+    this function fits logistic regression, MLP, and random forest 
+    and returns the 3 models. 
+    
+    inputs:
+        - x_train, y_train, x_test, y_test 
+        
+    outputs:
+        - logistic regression model, MLP model, and RF model
+    
+    '''
+    
+    logisticRegr = LogisticRegression()
+    logisticRegr.fit(x_train, y_train)
+    
+    clf = MLPClassifier(random_state=1, max_iter=300).fit(x_train, y_train)
+
+    RF = RandomForestClassifier()
+    RF.fit(x_train,y_train)
+    
+    return logisticRegr, clf, RF
+    
+    
+from sklearn import datasets, metrics, model_selection, svm
+def choose_best_model(logisticRegr, clf, RF):
+    '''
+    this function chooses the model with the highest accuracy and returns it
+    
+    inputs:
+        - 3 models: logisticRegr, clf, RF
+    outputs:
+        - model with best accuracy
+    
+    '''
+    models = [logisticRegr, clf, RF]
+    log_test_accuracy = logisticRegr.score(x_test, y_test)
+    clf_test_accuracy = clf.score(x_test, y_test)
+    rf_test_accuracy = RF.score(x_test, y_test)
+    
+    accuracies = [log_test_accuracy, clf_test_accuracy, rf_test_accuracy]
+    best_accuracy = np.max(accuracies)
+    best_model = models[np.argmax(accuracies)]
+
+    best_test_predictions = best_model.predict(x_test)
+   
+    
+    model_names = ['logistic regression', 'mlp', 'random forest']
+    best_model_name = model_names[np.argmax(accuracies)]
+    
+    
+	
+    return best_model, best_test_predictions, best_model_name
+    
+
+	
+def graph_confusion_roc(best_model, x_test, y_test, best_test_predictions, best_model_name):
+    '''
+    this function
+    creates graphs of roc curve and confusion matrix for the best model
+    
+    inputs:
+        - best_model = given by choose_best_model
+		- best_test_predictions = list of test predictions given by best model
+		- best_model_name = str of name of best model
+    outputs:
+        - graph of roc curve, saved to ./figures
+        - graph of confusion matrix, saved to ./figures
+
+    '''
+
+
+    print('Best Model is: {0}'.format(best_model_name))
+    cm = metrics.confusion_matrix(y_test, best_test_predictions)
+    
+    plt.figure(figsize=(9,9))
+    sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r')
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+    all_sample_title = 'Accuracy Score of Best Model: {0}'.format(score)
+    plt.title(all_sample_title, size = 15)
+    plt.savefig('../figures/'+ best_model_name + '_confusion_matrix')
+    plt.show()
+
+    
+    fpr, tpr, thresholds = metrics.roc_curve(list(y_test), best_test_predictions)
+    print( "Best model AUC is: {0}".format(metrics.auc(fpr, tpr)))
+    plt.plot(fpr, tpr)
+    plt.title("ROC curve")
+    plt.xlabel("TPR")
+    plt.ylabel("FPR")
+    plt.savefig('../figures/'+ best_model_name + '_ROC_curve')
+    plt.show()
+    
